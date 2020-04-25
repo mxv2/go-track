e0e5fd277b14d8c0270fb395e00baa6646c5e809
mxv2 <mxvkiryanov@gmail.com>
2020-04-25 20:51:07 +0300

    proverb: First iteration
    BenchmarkProverb-8	734298	1620 ns/op	1008 B/op	22 allocs/op

---
+++ proverb/proverb.go
@@ -0,0 +1,18 @@
+// Package proverb provides proverbs generator.
+package proverb
+
+// Proverb returns rhyme strings from 'For Want of a Nail'.
+func Proverb(rhyme []string) []string {
+	if len(rhyme) == 0 {
+		return []string{}
+	}
+
+	proverb := make([]string, 0, len(rhyme))
+	for i := 0; i < len(rhyme)-1; i += 1 {
+		str := "For want of a " + rhyme[i] + " the " + rhyme[i+1] + " was lost."
+		proverb = append(proverb, str)
+	}
+
+	proverb = append(proverb, "And all for the want of a "+rhyme[0]+".")
+	return proverb
+}
