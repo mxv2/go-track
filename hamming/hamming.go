8f5e75db23847cb6693148e2337cbc88d7222cd0
mxv2 <mxvkiryanov@gmail.com>
2020-04-30 11:01:28 +0300

    hamming: Fix linter errors

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,6 +3,8 @@ package hamming
 
 import "errors"
 
+// Distance returns calculated Hamming distance between two words.
+// An error is returned if words have not equal length.
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, errors.New("words not equal")
@@ -11,7 +13,7 @@ func Distance(a, b string) (int, error) {
 	distance := 0
 	for i := range a {
 		if a[i] != b[i] {
-			distance += 1
+			distance++
 		}
 	}
 
