8b3f564db7ada140bb48b5bfba85c50230332c08
mxv2 <mxvkiryanov@gmail.com>
2020-06-03 22:04:29 +0300

    roman-numerals: Refactor
    Split numerals to ranks and subranks.
    Subranks describe special cases, like 900 = CM.

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -14,6 +14,9 @@ var table = map[int]string{
 	1:    "I",
 }
 
+var ranks = []int{1000, 500, 100, 50, 10, 5, 1}
+var subRanks = []int{100, 100, 10, 10, 1, 1, 1}
+
 // ToRomanNumeral converts `n` to roman number as string.
 // Returns error when number does not satisfy range (1 to 3000).
 func ToRomanNumeral(n int) (string, error) {
@@ -22,48 +25,23 @@ func ToRomanNumeral(n int) (string, error) {
 	}
 
 	res := ""
-	for n > 0 {
-		switch {
-		case n >= 1000:
-			n -= 1000
-			res += table[1000]
-		case n-900 >= 0 && n-900 < 100:
-			n -= 900
-			res += table[100] + table[1000]
-		case n >= 500:
-			n -= 500
-			res += table[500]
-		case n-400 >= 0 && n-400 < 100:
-			n -= 400
-			res += table[100] + table[500]
-		case n >= 100:
-			n -= 100
-			res += table[100]
-		case n-90 >= 0 && n-90 < 10:
-			n -= 90
-			res += table[10] + table[100]
-		case n >= 50:
-			n -= 50
-			res += table[50]
-		case n-40 >= 0 && n-40 < 10:
-			n -= 40
-			res += table[10] + table[50]
-		case n >= 10:
-			n -= 10
-			res += table[10]
-		case n-9 >= 0 && n-9 < 1:
-			n -= 9
-			res += table[1] + table[10]
-		case n >= 5:
-			n -= 5
-			res += table[5]
-		case n-4 >= 0 && n-4 < 1:
-			n -= 4
-			res += table[1] + table[5]
-		case n >= 1:
-			n--
-			res += table[1]
+	for i := 0; n > 0; {
+		rnk := ranks[i]
+		if n >= rnk {
+			n -= rnk
+			res += table[rnk]
+			continue
 		}
+
+		srnk := subRanks[i]
+		specialCase := rnk - srnk
+		if n-specialCase >= 0 && n-specialCase < srnk {
+			n -= specialCase
+			res += table[srnk] + table[rnk]
+			continue
+		}
+
+		i++
 	}
 
 	return res, nil
