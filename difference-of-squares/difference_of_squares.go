34b489e8ed5cce1829385d42c4c9e1a70ca4aca5
mxv2 <mxvkiryanov@gmail.com>
2020-07-07 00:35:29 +0300

    difference-of-squares: Use Gauss formulas

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,22 +1,16 @@
 // Package diffsquares solves math square problems.
+// Under the hood, implementations use fast Gauss formulas.
 package diffsquares
 
 // SquareOfSum calculates square of sum for numbers until n.
 func SquareOfSum(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
+	sum := n * (n + 1) / 2
 	return sum * sum
 }
 
 // SumOfSquares calculates sum of squares for numbers until n.
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-	return sum
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference calculates subtraction of SquareOfSum and SumOfSquares results for numbers until n.
